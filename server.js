6bc27da13a054e636fb45440c1fcf047db472856
Harsh Nagpal <harshnagpal2502@gmail.com>
2022-08-27 17:22:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,18 @@ app.use(express.static(path.join(__dirname,'public')));
 //Runs when a Client Connects
 io.on('connection',socket=>{
   console.log('New Connection');
+  
+  //Welcome Current User
   socket.emit('message','Welcome to ChatCord!')
+
+  //Broadcast when a user connects
+  socket.broadcast.emit('message','A user has joined the chat');//Everyone expect the user that is connecting
+
+  //Runs when the client disconnects
+  socket.on('disconnect',()=>{
+  io.emit('message','A user has left the chat')
+  })
+
 })
 
 const PORT=3000 || process.env.PORT;
